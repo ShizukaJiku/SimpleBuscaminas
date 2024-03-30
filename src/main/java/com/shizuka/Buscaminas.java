6370adb6c558bee359ecd0c134772f0c4f9b7362
shizu <shizuka.jiku@gmail.com>
2024-03-29 23:10:55 -0500

    Generacion de minas y guias

--- src/main/java/com/shizuka/Buscaminas.java
+++ src/main/java/com/shizuka/Buscaminas.java
@@ -1,2 +1,80 @@
-package com.shizuka;public class Buscaminas {
+package com.shizuka;
+
+import java.util.Random;
+
+public class Buscaminas {
+    private final int size;
+    private final int cantMines;
+    private final int[][] table;
+
+    public Buscaminas(int size, int cantMines) {
+        this.size = size;
+        this.cantMines = cantMines;
+        this.table = new int[size][size];
+        generateTable();
+    }
+
+    private void generateTable() {
+        generateMines();
+        generateGuides();
+    }
+
+    private void generateMines() {
+        Random random = new Random();
+        int minesPlaced = 0;
+
+        while (minesPlaced < cantMines) {
+            int x = random.nextInt(size);
+            int y = random.nextInt(size);
+
+            if (table[x][y] == 0) {
+                table[x][y] = -1;
+                minesPlaced++;
+            }
+        }
+    }
+
+    private void generateGuides(){
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                if(table[x][y] == -1) addGuide(x, y);
+            }
+        }
+    }
+
+    private void addGuide(int x, int y) {
+        int[] dx = {-1, 1, 0, 0, -1, -1, 1, 1};
+        int[] dy = {0, 0, -1, 1, -1, 1, -1, 1};
+
+        for (int i = 0; i < 8; i++) {
+            tryAddGuide(x + dx[i], y + dy[i]);
+        }
+    }
+
+    private void tryAddGuide(int x, int y) {
+        if (!isValidCoordinate(x, y)) return;
+        if (table[x][y] == -1) return;
+        table[x][y]++;
+    }
+
+    private boolean isValidCoordinate(int x, int y) {
+        return x >= 0 && x < size && y >= 0 && y < size;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int x = 0; x < size; x++) {
+            for (int y = 0; y < size; y++) {
+                var value = table[x][y] == -1 ? 'M' : String.valueOf(table[x][y]);
+                stringBuilder.append(value);
+
+                if(y != size - 1) stringBuilder.append(' ');
+            }
+            stringBuilder.append('\n');
+        }
+
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/com/shizuka/Main.java
+++ src/main/java/com/shizuka/Main.java
@@ -2,6 +2,8 @@ package com.shizuka;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Generar buscaminas simple xDxD");
+        Buscaminas buscaminas = new Buscaminas(5, 6);
+        System.out.println(buscaminas);
     }
 }
